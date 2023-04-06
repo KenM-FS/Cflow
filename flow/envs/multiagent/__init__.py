19dbaa0a5a35e0889ffa422805364412beefed20
Ken Matsuda <g2122063@osawanoiMac-2.local>
2023-04-06 15:23:47 +0900

    Clean ring_highway CommEnv to inherit NcomEnv

--- flow/envs/multiagent/__init__.py
+++ flow/envs/multiagent/__init__.py
@@ -11,7 +11,7 @@ from flow.envs.multiagent.traffic_light_grid import MultiTrafficLightGridPOEnv
 from flow.envs.multiagent.highway import MultiAgentHighwayPOEnv
 from flow.envs.multiagent.merge import MultiAgentMergePOEnv
 from flow.envs.multiagent.i210 import I210MultiEnv
-from flow.envs.multiagent.ring_highway import MultiAgentRingHighwayPOEnv, MultiAgentRingHighwayPONcomEnv
+from flow.envs.multiagent.ring_highway import MultiAgentRingHighwayPOCommEnv, MultiAgentRingHighwayPONcomEnv
 
 __all__ = [
     'MultiEnv',
