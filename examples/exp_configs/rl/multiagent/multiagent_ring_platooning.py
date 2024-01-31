97d51b75415ed9d5eaa6bdc3bc803111e609565e
Ken Matsuda <g2122063@osawanoiMac-2.local>
2024-01-31 11:22:20 +0900

    Last commit

--- examples/exp_configs/rl/multiagent/multiagent_ring_platooning.py
+++ examples/exp_configs/rl/multiagent/multiagent_ring_platooning.py
@@ -14,11 +14,11 @@ from flow.envs.multiagent import MultiAgentRingHighwayPONcomEnv
 from flow.networks import RingNetwork
 from flow.utils.registry import make_create_env
 
-EXP_TAG = "platooning"
-VERSION = 1
+EXP_TAG = "platooning_test"
+VERSION = 2
 RENDER = False
-ENV_NAME = MultiAgentRingHighwayPOCommEnv
-# ENV_NAME = MultiAgentRingHighwayPONcomEnv
+# ENV_NAME = MultiAgentRingHighwayPOCommEnv
+ENV_NAME = MultiAgentRingHighwayPONcomEnv
 
 RATE_CAV = 0.3
 NUM_LANE = 3
@@ -41,27 +41,42 @@ set_human = set_lane - set_cav
 set_human_remaining = set_human
 
 vehicles = VehicleParams()
-for i in range(set_cav):
-    # Add one automated vehicle.
-    vehicles.add(
-        veh_id="rl_{}".format(i),
-        acceleration_controller=(RLController, {}),
-        routing_controller=(ContinuousRouter, {}),
-        num_vehicles=3
-    )
+# for i in range(set_cav):
+#     # Add one automated vehicle.
+#     vehicles.add(
+#         veh_id="rl_{}".format(i),
+#         acceleration_controller=(RLController, {}),
+#         routing_controller=(ContinuousRouter, {}),
+#         num_vehicles=3
+#     )
 
-    # Add a fraction of the remaining human vehicles.
-    sets_to_add = round(set_human_remaining / (set_cav - i))
-    set_human_remaining -= sets_to_add
+#     # Add a fraction of the remaining human vehicles.
+#     sets_to_add = round(set_human_remaining / (set_cav - i))
+#     set_human_remaining -= sets_to_add
 
-    vehicles.add(
-        veh_id="human_{}".format(i),
-        acceleration_controller=(IDMController, {
-            "noise": 7.0
-        }),
-        lane_change_params=SumoLaneChangeParams(lane_change_mode="sumo_default"),
-        routing_controller=(ContinuousRouter, {}),
-        num_vehicles=sets_to_add*3)
+#     vehicles.add(
+#         veh_id="human_{}".format(i),
+#         acceleration_controller=(IDMController, {
+#             "noise": 7.0
+#         }),
+#         lane_change_params=SumoLaneChangeParams(lane_change_mode="sumo_default"),
+#         routing_controller=(ContinuousRouter, {}),
+#         num_vehicles=sets_to_add*3)
+vehicles.add(
+    veh_id="rl",
+    acceleration_controller=(RLController, {}),
+    routing_controller=(ContinuousRouter, {}),
+    num_vehicles=num_cav
+)
+vehicles.add(
+    veh_id="human",
+    acceleration_controller=(IDMController, {
+        "noise": 7.0
+    }),
+    lane_change_params=SumoLaneChangeParams(lane_change_mode="sumo_default"),
+    routing_controller=(ContinuousRouter, {}),
+    num_vehicles=NUM_TOTAL - num_cav
+)
 
 
 flow_params = dict(
